43965f93ecdcd3f4297acb388d47fbb7243c6652
Olawale <olawalesodiq988@gmail.com>
2026-02-20 22:43:12 +0100

    Still having issues with the file path

---
+++ src/Components/Product-View/product-view.jsx
@@ -0,0 +1,27 @@
+
+function ModalView(){
+    retutn(
+        <>
+        <div>
+        <div>
+<img src="" alt="" />
+        </div>
+<div>
+    <img src="" alt="" />
+    <img src="" alt="" />
+    <img src="" alt="" />
+    <img src="" alt="" />
+</div>
+
+
+
+
+
+        </div>
+        
+        
+        </>
+    )
+}
+
+export default ModalView;
--- src/Components/Product-thumbnails/product-thumbnail.jsx
+++ src/Components/Product-thumbnails/product-thumbnail.jsx
@@ -1,15 +1,28 @@
-import "/src/Components/Product-thumbnails/product-thumbnails.css";
+import '/src/Components/Product-thumbnails/product-thumbnails.css';
+import { useState } from "react";
+// import {ModalView} from "./src/Components/Product-View/product-view.jsx"
 
 
 function ImageThumbnails() {
+const [isModalOn, setIsModalOn] = useState(null);
+
+
+function handleModalViewDisplay() {
+    setIsModalOn(!isModalOn)
+}
+
+
+
     return(<>
     <div className="thumb-images">
-    <img src="public/image-product-1.jpg" alt="" />
-    <img src="public/image-product-2.jpg" alt="" />
-    <img src="public/image-product-3.jpg" alt="" />
-    <img src="public/image-product-4.jpg" alt="" />
+    <img src="/public/image-product-1.jpg" alt="" onClick={handleModalViewDisplay} />
+    <img src="/public/image-product-2.jpg" alt="" onClick={handleModalViewDisplay}/>
+    <img src="/public/image-product-3.jpg" alt="" onClick={handleModalViewDisplay}/>
+    <img src="/public/image-product-4.jpg" alt="" onClick={handleModalViewDisplay}/>
 
     </div>
+
+    {/* {isModalOn && <ModalView /> } */}
     
     
     </>)

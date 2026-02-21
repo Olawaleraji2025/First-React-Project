c2abbe212fe7db7437668bb7d32a146bd0927a98
Olawale <olawalesodiq988@gmail.com>
2026-02-21 13:47:17 +0100

    changed image path

--- src/Components/Product-View/product-view.jsx
+++ src/Components/Product-View/product-view.jsx
@@ -7,10 +7,10 @@ export function ModalView(){
 <img src="../image-product-1-thumbnail.jpg" alt="" />
         </div>
 <div>
-    <img src="../image-product-1-thumbnail.jpg" alt="" />
-    <img src="../image-product-2-thumbnail.jpg" alt="" />
-    <img src="../image-product-3-thumbnail.jpg" alt="" />
-    <img src="../image-product-4-thumbnail.jpg" alt="" />
+    <img src="/image-product-1-thumbnail.jpg" alt="" />
+    <img src="/image-product-2-thumbnail.jpg" alt="" />
+    <img src="/image-product-3-thumbnail.jpg" alt="" />
+    <img src="/image-product-4-thumbnail.jpg" alt="" />
 </div>
 
 
